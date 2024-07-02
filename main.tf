2ba11f3fb9af43076fafb85fdf4b94a65b95c474
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2024-07-02 18:01:05 +0000

    Attempting to convert to Kubernetes

--- main.tf
+++ main.tf
@@ -17,11 +17,10 @@ resource "hcloud_server" "jump-server" {
   server_type = "cx21"
   datacenter  = "hel1-dc2"
   ssh_keys    = ["My SSH KEY"]
-  public_net  {
+  public_net {
     ipv4_enabled = true
     ipv6_enabled = false
   }
-
 
   network {
     network_id = hcloud_network.kubernetes-node-network.id
@@ -84,22 +83,45 @@ resource "hcloud_server" "jump-server" {
   }
 }
 
-# Create the kubernetes nodes
-resource "hcloud_server" "kube-node" {
+# Create the kubernetes master nodes
+resource "hcloud_server" "kube-master" {
   count       = 1
-  name        = "kube-node-${count.index + 1}"
+  name        = "kube-master-${count.index + 1}"
   image       = "ubuntu-22.04"
   server_type = "cx11"
   datacenter  = "hel1-dc2"
   ssh_keys    = ["My SSH KEY", "JUMP SERVER SSH KEY"]
-  public_net  {
+  public_net {
     ipv4_enabled = true
     ipv6_enabled = false
   }
 
   network {
     network_id = hcloud_network.kubernetes-node-network.id
-    ip         = "172.16.0.10${count.index + 1}"
+    ip         = "172.16.0.20${count.index + 1}"
+  }
+
+  depends_on = [
+    hcloud_network_subnet.kubernetes-node-subnet
+  ]
+}
+
+# Create the kubernetes nodes
+resource "hcloud_server" "kube-worker" {
+  count       = 1
+  name        = "kube-worker-${count.index + 1}"
+  image       = "ubuntu-22.04"
+  server_type = "cx11"
+  datacenter  = "hel1-dc2"
+  ssh_keys    = ["My SSH KEY", "JUMP SERVER SSH KEY"]
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = false
+  }
+
+  network {
+    network_id = hcloud_network.kubernetes-node-network.id
+    ip         = "172.16.0.30${count.index + 1}"
   }
 
   depends_on = [
@@ -113,7 +135,6 @@ resource "hcloud_network" "kubernetes-node-network" {
   ip_range = "172.16.0.0/24"
 }
 
-
 # Create the node subnet
 resource "hcloud_network_subnet" "kubernetes-node-subnet" {
   type         = "cloud"
@@ -123,10 +144,33 @@ resource "hcloud_network_subnet" "kubernetes-node-subnet" {
 }
 
 resource "local_file" "hosts_cfg" {
-    content = templatefile("server/inventory.tmpl", 
-        {
-            ubuntu_workers = hcloud_server.kube-node
-        }
-    )
-    filename = "server/inventory"
+  content = templatefile("server/inventory.tmpl", {
+    jump_server_ip = flatten([for net in tolist(hcloud_server.jump-server.network) : net.ip])[0]
+    masters = [for master in hcloud_server.kube-master : {
+      ip = flatten([for net in tolist(master.network) : net.ip])[0]
+    }]
+    workers = [for worker in hcloud_server.kube-worker : {
+      ip = flatten([for net in tolist(worker.network) : net.ip])[0]
+    }]
+    jump_server_public_ip = hcloud_server.jump-server.ipv4_address
+  })
+  filename = "server/inventory"
+}
+
+# Add provisioner to copy the inventory file to jump-server
+resource "null_resource" "copy_inventory_to_jump_server" {
+  depends_on = [local_file.hosts_cfg]
+
+  provisioner "file" {
+    source      = "${local_file.hosts_cfg.filename}"
+    destination = "/root/.server/inventory"
+
+    connection {
+      type        = "ssh"
+      host        = hcloud_server.jump-server.ipv4_address
+      private_key = file("~/.ssh/devtools-kube")
+      user        = "root"
+      timeout     = "2m"
+    }
+  }
 }
--- output.tf
+++ output.tf
@@ -2,8 +2,3 @@
 output "jump-server-ip" {
  value = [hcloud_server.jump-server.ipv4_address]
 }
-
-# Node IPs
-# output "kube-node-1-ip" {
-#  value = [for node in hcloud_server.kube-node[0].network: node.ip]
-# }
