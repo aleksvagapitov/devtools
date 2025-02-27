4be072092de10dea5192aac90e1a3f6b2eec7678
Aleksandr Agapitov <aleksvagapitov@gmail.com>
2025-02-27 19:46:05 +0000

    Removing argocd, fixing jenkins docker image

--- scripts/infra-up.sh
+++ scripts/infra-up.sh
@@ -13,7 +13,7 @@ kubectl create namespace logging
 
 # Start certificate management service
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.1/cert-manager.yaml
-sleep 15
+sleep 20
 kubectl apply -f cert-manager.yaml
 
 # Start registry
@@ -36,12 +36,15 @@ kubectl apply -f jenkins-ingress.yaml
 sleep 15
 kubectl cp infra/$(kubectl get pods -n infra -l app=jenkins -o jsonpath="{.items[0].metadata.name}"):var/jenkins_home/secrets/initialAdminPassword ~/jenkinsPassword
 
-# Install ArgoCD
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl wait --for=condition=Ready pod -n argocd -l app.kubernetes.io/name=argocd-server --timeout=300s
-kubectl patch configmap argocd-cm -n argocd --type merge -p '{"data":{"server.insecure": "true"}}'
-kubectl rollout restart deployment argocd-server -n argocd
-kubectl apply -f argocd-ingress.yaml
+# Install Prometheus
+kubectl apply --server-side -f https://github.com/prometheus-operator/prometheus-operator/releases/latest/download/bundle.yaml
+kubectl apply -f prometheus.yaml
+kubectl apply -f prometheus-ingress.yaml
+
+# Install Grafana
+kubectl apply -f grafana.yaml
+kubectl wait --for=condition=Ready pod -n monitoring -l app=grafana --timeout=300s
+kubectl apply -f grafana-ingress.yaml
 
 # Install Loki
 kubectl apply -f loki.yaml 
@@ -53,15 +56,5 @@ kubectl create namespace observability
 kubectl apply -f https://github.com/jaegertracing/jaeger-operator/releases/download/v1.45.0/jaeger-operator.yaml -n observability
 kubectl apply -f jaeger.yaml
 kubectl apply -f jaeger-ingress.yaml
-
-# Install Prometheus
-kubectl apply --server-side -f https://github.com/prometheus-operator/prometheus-operator/releases/latest/download/bundle.yaml
-kubectl apply -f prometheus.yaml
-kubectl apply -f prometheus-ingress.yaml
-
-# Install Grafana
-kubectl apply -f grafana.yaml
-kubectl wait --for=condition=Ready pod -n monitoring -l app=grafana --timeout=300s
-kubectl apply -f grafana-ingress.yaml
 
 echo "Setup complete."
